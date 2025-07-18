71bfdc46593c9a2dbc45d67f904c694512635fed
Synopsik <synopsikk@gmail.com>
2025-07-18 00:50:07 -0700

    Update: Added health POST endpoint and updated name for health GET endpoint

--- image/api/endpoints/health.py
+++ image/api/endpoints/health.py
@@ -2,7 +2,14 @@ from fastapi import APIRouter
 
 router = APIRouter(prefix="/health", tags=["stats"])
 
+
 @router.get("/")
-async def query():
+async def health_get() -> dict:
     return {"message": "Entry for health check endpoint"}
 
+@router.post("/")
+async def health_post(status: int) -> int:
+    if status == 1:
+        return 1
+    else:
+        return 0
