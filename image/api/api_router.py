5cf72bf1ce0ed7a6f8fae37044a8951b28ea02cd
Synopsik <synopsikk@gmail.com>
2025-07-18 00:56:39 -0700

    Update: Added reset endpoint to router

--- image/api/api_router.py
+++ image/api/api_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from endpoints import health, query 
+from endpoints import health, query, reset
 
 api_router = APIRouter()
+api_router.include_router(reset.router)
 api_router.include_router(health.router)
 api_router.include_router(query.router)
