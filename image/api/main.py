cd9d8613cd7c4f8c9ba16df31d4efe65d0f84bd8
Synopsik <synopsikk@gmail.com>
2025-07-18 00:57:43 -0700

    Update: Updating API to use router, mangum, and uvicorn

--- image/api/main.py
+++ image/api/main.py
@@ -1,4 +1,25 @@
+from fastapi import FastAPI
+from mangum import Mangum
+from api_router import api_router
 
-from start import app
+app = FastAPI(
+    title="AWS Discord Bot",
+    version="1.0.0",
+    # So much more to add
+)
 
+app.include_router(api_router)
 
+@app.get("/")
+async def root():
+    return {"response": "Go away"}
+
+handler = Mangum(app)
+
+'''
+'''
+if __name__ == "__main__": # Comment out in prod
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+'''
+'''
