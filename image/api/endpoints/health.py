43c8ac8a54a024909e6c9bdd11e4b2d809600bfb
Synopsik <synopsikk@gmail.com>
2025-07-20 16:25:22 -0700

    Update: Changed to GET endpoint
    Different /index/ paths return different checks. Need to change this to just use a passed health item

--- image/api/endpoints/health.py
+++ image/api/endpoints/health.py
@@ -3,13 +3,10 @@ from fastapi import APIRouter
 router = APIRouter(prefix="/health", tags=["stats"])
 
 
-@router.get("/")
-async def health_get() -> dict:
-    return {"message": "Entry for health check endpoint"}
-
-@router.post("/")
-async def health_post(status: int) -> int:
-    if status == 1:
-        return 1
-    else:
-        return 0
+@router.get("/{index}")
+async def health_get(index) -> dict:
+    match int(index):
+        case 1:
+            return {"health": "Healthy"}
+        case _:
+            return {"health": "Unhealthy"}
