a302be4e66a3e514d6200d710a21f3118dc92291
Synopsik <synopsikk@gmail.com>
2025-07-20 23:25:30 -0700

    Update: Added Agent endpoint route

--- image/api/util/api_router.py
+++ image/api/util/api_router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from endpoints import health, query, reset
+from endpoints import health, query, reset, agent
 
 api_router = APIRouter()
 api_router.include_router(reset.router)
 api_router.include_router(health.router)
 api_router.include_router(query.router)
+api_router.include_router(agent.router)
